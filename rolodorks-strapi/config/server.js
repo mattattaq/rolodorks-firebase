28e6d98dbd42f0c1e62c436707bcffdf8438fe66
MattAttaq <mattatttaq@gmail.com>
2020-09-23 00:08:03 -0400

    yarn lock and basic strapi install
    yarn lock and basic strapi install

---
+++ rolodorks-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a61fcd6416b91ec623ffbb8a809bb0af'),
+    },
+  },
+});
